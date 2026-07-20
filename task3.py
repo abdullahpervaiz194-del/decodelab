418cbe98845d8c99446602f683a02d60f7ea0e3e
abdullahpervaiz194-del <abdullahpervaiz194@gmail.com>
2026-07-20 22:48:32 +0500

    Random password generator

--- task3.py
+++ task3.py
@@ -1,5 +1,5 @@
 import random
-import string
+import string 
 length=int(input("Enter the length of the password "))
 password=''.join(random.choices(string.ascii_letters + string.digits, k=length))
 print("Generated password: ",password)
